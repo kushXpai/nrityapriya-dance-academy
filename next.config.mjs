ab4b0374c06f9642cf4b23084f3bdd4ae1cb61d8
Kush Pai <kush060503@gmail.com>
2025-01-10 19:09:13 +0530

    Updated next.config.mjs to add domain for image

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    domains: ['nrityapriyadanceacademy.vercel.app'],
+    unoptimized: true,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
